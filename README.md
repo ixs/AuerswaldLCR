492765a8e79ba1fb01e378a7191d3e46d6e7117c
Andreas Thienemann <andreas@bawue.net>
2024-04-20 19:36:12 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # AuerswaldLCR
-Update the Least-Cost-Router tables in an Auerswald PBX system
+
+Update the Least-Cost-Router tables in an Auerswald PBX system.
+
+Current Auerswald PBX system offer a least-cost-router that allows to route calls via various providers.
+
+This is a client for the Soft-LCR 4.0 interface.
+
+[Telefonsparbuch](http://www.telefonsparbuch.de) XML files are compatible and can be imported into the PBX.  
+This has been tested with the [AuerswaldVOIP](http://www.telefonsparbuch.de/tmpl/calc/telephone/lcr/AuerswaldVoIP/calc_tk.htm)
+type.
+
+## Installation
+
+Clone the repo, install the requirements using pip:
+
+```
+git clone https://github.com/ixs/AuerswaldLCR.git
+cd AuerswaldLCR
+pip install -r requirements.txt
+```
+
+## Configuration
+
+Rename `auerswald.cfg.yaml.sample` to `auerswald.cfg.yaml` and add your site specific details.
+
+## Usage
+
+`./auerswald-lcr.py --help` for help output.  
+`./auerswald-lcr.py download [filename]` to download the LCR XML from the PBX.  
+`./auerswald-lcr.py --pretty download [filename]` to download the LCR XML from the PBX and prettify it.  
+`./auerswald-lcr.py upload [filename]` to upload a LCR XML to the PBX.  
+`./auerswald-lcr.py defaults` to reset the LCR XML back to defaults.  
+`./auerswald-lcr.py erase` flush all LCR tables on the PBX.
+
+In case of trouble, the `--debug` option will enable logging of all data sent over the wire.
---
+++ auerswald-lcr.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python3
+"""
+AuerswaldLCR
+Update the Least-Cost-Router tables in an Auerswald PBX system
+
+Author: Andreas Thienemann
+License: GPLv3+
+"""
+
+import argparse
+import gzip
+import requests
+import urllib.parse
+import logging
+import xml.dom.minidom
+import yaml
+
+
+class AuerswaldLCR:
+    """Auerswald LCR client"""
+
+    def __init__(self):
+        self.ssl_verify = False
+        self.uuid = None
+        self._load_config()
+        self.session = requests.Session()
+        self.session.auth = requests.auth.HTTPDigestAuth(
+            self.auer_admin_user, self.auer_admin_pass
+        )
+        self.session.verify = False
+
+        # Silence warnings if we're not doing verification
+        if not self.ssl_verify:
+            from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+            requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+    def _load_config(self, file="auerswald.cfg.yaml"):
+        """Load the configfile"""
+        with open(file, "r") as f:
+            data = yaml.safe_load(f)
+        self.auer_address = data["auer_address"]
+        self.auer_admin_user = data["auer_admin_user"]
+        self.auer_admin_pass = data["auer_admin_pass"]
+
+    def _enable_debug(self):
+        """Enable debug output for requests"""
+        try:
+            import http.client as http_client
+        except ImportError:
+            # Python 2
+            import httplib as http_client
+        http_client.HTTPConnection.debuglevel = 1
+
+        # You must initialize logging, otherwise you'll not see debug output.
+        logging.basicConfig()
+        logging.getLogger().setLevel(logging.DEBUG)
+        requests_log = logging.getLogger("requests.packages.urllib3")
+        requests_log.setLevel(logging.DEBUG)
+        requests_log.propagate = True
+
+    def _fetch(self, path):
+        """Fetch generic data drom the PBX"""
+        r = self.session.get(f"https://{self.auer_address}{path}")
+        return r
+
+    def _send(self, path, params=None, data=None, files=None):
+        """Post generic data to the PBX"""
+        r = self.session.post(
+            f"https://{self.auer_address}{path}",
+            params=params,
+            data=data,
+            files=files,
+        )
+        return r
+
+    def fetch_max_values(self):
+        pass
+
+    def fetch_lcr_table(self):
+        """Fetch list of LCR tables"""
+        return self._fetch("/lcr35tabellen_list").json()
+
+    def fetch_lcr_provider(self):
+        """Fetch list of LCR providers"""
+        return self._fetch("/lcr35tabellen_provider").json()
+
+    def fetch_lcr_networks(self):
+        """Fetch list of LCR networks (Gasssen)"""
+        return self._fetch("/lcr35netze_state").json()
+
+    def fetch_lcr_table_info(self, netid, category, pageindex):
+        """Fetch of LCR networks details"""
+        params = {
+            "netzid": netid,
+            "category": category,
+            "pageindex": pageindex,
+        }
+        return self._fetch("/lcr35tabellen_state", params=params).json()
+
+    def download_lcr_xml(self):
+        """Download the LCR Table XML Export"""
+        return gzip.decompress(self._fetch("/lcr35datensicherung_export").content)
+
+    def set_lcr_defaults(self):
+        """Reset the LCR to defaults"""
+        return self._send("/lcr35datensicherung_default")
+
+    def upload_lcr_xml(self, filename, content):
+        """Upload and import new LCR XML table"""
+        url = "/lcr35datensicherung_import"
+        params = {"h_ulfilename": filename}
+        files = {"file": (filename, content, "text/xml")}
+        r = self._send(url, params=params, files=files)
+        return r.json()
+
+    def erase_table(self, tbl):
+        """Empty existing LCR tables"""
+        tables = {
+            "networks": "lcr35netze",
+            "providers": "lcr35provider",
+        }
+        self.erase_single_lcr_table(path=f"/{tables[tbl]}")
+
+    def erase_single_lcr_table(self, path):
+        """Erase all entries from a single LCR table"""
+        existing_data = self._fetch(f"{path}_state").json()
+        path = f"{path}_save"
+        data = {"ids": []}
+        for e in existing_data["rows"]:
+            id = e["id"]
+            data["ids"].append(str(id))
+            data[f"{id}_gr_id"] = id
+            data[f"{id}_!nativeeditor_status"] = "deleted"
+        data["ids"] = ",".join(data["ids"])
+        return self._send(path, data=urllib.parse.urlencode(data, safe="!"))
+
+    def download_xml(self, filename, pretty=False):
+        """Cmdline handler for downloading XML"""
+        xml_content = self.download_lcr_xml()
+        with open(filename, "w") as f:
+            if pretty:
+                lcr = xml.dom.minidom.parseString(xml_content)
+                # Write out the prettyfied version, we need to add a newline after the <?xml
+                # header though.
+                f.write(
+                    lcr.toprettyxml(indent="  ", newl="").replace(
+                        '<?xml version="1.0" ?>', '<?xml version="1.0" ?>\n'
+                    )
+                )
+            else:
+                f.write(xml_content)
+
+    def upload_xml(self, filename):
+        """Cmdline handler for uploading XML"""
+        with open(filename, "rb") as f:
+            r = self.upload_lcr_xml("lcr-autogen.xml", f.read())
+        if len(r["errors"]) > 0 or len(r["warnings"]) > 0:
+            print(r["errors"], r["warnings"])
+            exit(1)
+
+    def main(self):
+        parser = argparse.ArgumentParser(
+            description="Update the Least-Cost-Router tables in an Auerswald PBX system"
+        )
+        parser.add_argument(
+            "command",
+            choices=["upload", "download", "defaults", "erase"],
+            help="File handling, whether to upload or download the XML file.",
+        )
+        parser.add_argument("--debug", action="store_true", help="Debug output.")
+        parser.add_argument(
+            "--pretty", action="store_true", help="Prettify XML file when downloading."
+        )
+        parser.add_argument("filename", type=str, nargs="?", help="LCR XML file.")
+
+        args = parser.parse_args()
+
+        if args.debug:
+            self._enable_debug()
+
+        if args.command == "upload":
+            if args.filename:
+                self.upload_xml(args.filename)
+            else:
+                print("Error: Please provide a filename to upload.")
+        elif args.command == "download":
+            if args.filename:
+                self.download_xml(args.filename, args.pretty)
+            else:
+                print("Error: Please provide a filename to download.")
+        elif args.command == "defaults":
+            self.set_lcr_defaults()
+        elif args.command == "erase":
+            for tbl in ["networks", "providers"]:
+                self.erase_table(tbl)
+        else:
+            parser.print_help()
+
+
+if __name__ == "__main__":
+    aw_lcr = AuerswaldLCR()
+    aw_lcr.main()
